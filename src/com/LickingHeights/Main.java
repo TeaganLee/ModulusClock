04c06daa53a12b931b2ba9f55d38447cd1e374a5
Teagan Lee <teagan.lee.2020@student.lhschools.org>
2020-03-03 13:16:19 -0500

    UserdataCaptured

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,29 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ModulusClock();
+    }
+    public static void ModulusClock(){
+
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        int hour;
+        int minutes;
+
+        System.out.println("Hour:");
+        hour = keyboard.nextInt();
+
+        System.out.println("minute:");
+        minutes = keyboard.nextInt();
+
+
+
+    }
+
+}
